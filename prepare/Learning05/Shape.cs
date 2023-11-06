f0be2b878bd1bb1e735856e7b3316f9dec2ba814
uplandwave <113386040+uplandwave@users.noreply.github.com>
2023-11-06 12:27:18 -0700

    poly code added

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,15 @@
+public abstract class Shape{
+    private string _color;
+
+    public Shape(string color){
+        _color = color;
+    }
+    public string GitColor(){
+        return _color;
+    }
+
+    public void SetColor(string color){
+        _color = color;
+    }
+    public abstract double GetArea();
+}
